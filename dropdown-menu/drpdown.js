0ec29ae6d29aaae615acc5dba4d4e8f7d94117fd
DENSENSE <167194090+DENSENSE@users.noreply.github.com>
2024-10-26 11:31:51 +0300

    html, css, js dropdown-menu.
    Very clear view.

---
+++ dropdown-menu/drpdown.js
@@ -0,0 +1,17 @@
+function func1() {
+    document.getElementById('myDropdown').classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName('cntnt');
+        
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
